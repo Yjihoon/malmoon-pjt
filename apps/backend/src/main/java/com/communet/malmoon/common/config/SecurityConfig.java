610c835caa4d01e61db0c65266a44ab3d896c745
황성헌 <333dolphin@gmail.com>
2025-07-29 07:07:40 +0900

    Feat: 로그인 관련 기능
    엔드포인트 구현
    Spring Security 설정 구성
    JWT 토근 생성 및 유효성 검증 로직 구현
    UserDetailsService 구현
    
    #S13P11C107-191
    #S13P11C107-192
    #S13P11C107-193
    #S13P11C107-194

--- apps/backend/src/main/java/com/communet/malmoon/common/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/communet/malmoon/common/config/SecurityConfig.java
@@ -2,16 +2,55 @@ package com.communet.malmoon.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.communet.malmoon.member.jwt.auth.JwtAuthenticationFilter;
+import com.communet.malmoon.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final MemberRepository memberRepository;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+		AuthenticationManager authManager) throws Exception {
+
+		http
+			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.cors(AbstractHttpConfigurer::disable)
+			.addFilter(new JwtAuthenticationFilter(authManager, memberRepository))
+			.authorizeHttpRequests(auth -> auth
+				//.requestMatchers("/api/v1/users/me").authenticated()
+				.anyRequest().permitAll()
+			);
+
+		return http.build();
 	}
 }
