b3adffcd97fa3c3093ca7776a03386f1c5e774e8
Deokgyu IM <imdg3530@gmail.com>
2025-07-28 17:53:12 +0900

    Feat : 동화책 데이터, 문장 DB에 저장 #S13P11C107-127

---
+++ apps/backend/src/main/java/com/communet/malmoon/storybook/repository/StorybookRepository.java
@@ -0,0 +1,8 @@
+package com.communet.malmoon.storybook.repository;
+// JPA 인터페이스
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.communet.malmoon.storybook.domain.Storybook;
+
+public interface StorybookRepository extends JpaRepository<Storybook, Long> {
+}
