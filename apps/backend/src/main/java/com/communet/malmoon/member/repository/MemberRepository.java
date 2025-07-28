93c51806f8fd1d6f0448d75f27efff2fb2a18077
황성헌 <333dolphin@gmail.com>
2025-07-28 23:59:34 +0900

    Feat: 회원가입 테스트 코드 작성
    회원, 치료사 회원가입 테스트
    중복 이메일 가입 테스트
    
    #S13P11C107-118

--- apps/backend/src/main/java/com/communet/malmoon/member/repository/MemberRepository.java
+++ apps/backend/src/main/java/com/communet/malmoon/member/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.communet.malmoon.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.communet.malmoon.member.domain.Member;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
+	Optional<Member> findByEmail(String email);
 }
