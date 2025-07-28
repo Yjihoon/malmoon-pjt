96cd420aaa85bc3eaaffbf7967a5d2ee7db9e1dd
황성헌 <333dolphin@gmail.com>
2025-07-28 23:40:36 +0900

    Feat: 이메일 중복 체크 기능
    #S13P11C107-113

--- apps/backend/src/main/java/com/communet/malmoon/member/repository/MemberRepository.java
+++ apps/backend/src/main/java/com/communet/malmoon/member/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.communet.malmoon.member.repository;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.communet.malmoon.member.domain.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	boolean existsByEmail(String email);
 }
--- apps/backend/src/main/java/com/communet/malmoon/member/service/MemberService.java
+++ apps/backend/src/main/java/com/communet/malmoon/member/service/MemberService.java
@@ -1,4 +1,18 @@
 package com.communet.malmoon.member.service;
 
+import org.springframework.stereotype.Service;
+
+import com.communet.malmoon.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	public boolean checkEmail(String email) {
+		return memberRepository.existsByEmail(email);
+	}
 }
