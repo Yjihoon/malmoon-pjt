5c0388ace31408b48b910c2d6b8d594a3627ef1f
정수형 <reosis96@gmail.com>
2025-07-30 12:06:52 +0900

    feat: 예외 처리 리팩토링 (GlobalExceptionHandler) 및 ApiErrorRes, ExceptionR utils 추가

--- apps/backend/src/main/java/com/communet/malmoon/common/exception/GlobalExceptionHandler.java
+++ apps/backend/src/main/java/com/communet/malmoon/common/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package com.communet.malmoon.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
