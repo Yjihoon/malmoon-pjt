2ad08688bc7f3616bf7a18268e635b3d7d61ab2e
정형진 <gorani0724@gmail.com>
2025-08-04 17:14:14 +0900

    Fix: json 데이터 동기화

--- apps/backend/src/main/java/com/communet/malmoon/member/dto/request/CareerReq.java
+++ apps/backend/src/main/java/com/communet/malmoon/member/dto/request/CareerReq.java
@@ -1,15 +1,18 @@
 package com.communet.malmoon.member.dto.request;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class CareerReq {
     private String company;
     private String position;
+    @JsonProperty("start_date")
     private LocalDate startDate;
+    @JsonProperty("end_date")
     private LocalDate endDate;
 }
