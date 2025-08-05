1129792fda455defca9f2eed0e31ab04d83e346f
Deokgyu IM <imdg3530@gmail.com>
2025-08-05 11:39:34 +0900

    feat: GMS API를 통한 STT 처리 #S13P11C107-103

--- apps/AI/main.py
+++ apps/AI/main.py
@@ -2,6 +2,12 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from apps.AI.api.v1.aac_create import aac_router
+from apps.AI.api.v1.stt.stt_router import router as stt_router
+
+from dotenv import load_dotenv
+
+# 환경변수 로딩
+load_dotenv()
 
 app = FastAPI()
 
@@ -10,7 +16,9 @@ app.mount("/static", StaticFiles(directory="apps/AI/static"), name="static")
 
 # AAC 라우터 등록
 app.include_router(aac_router.router, prefix="/api/v1/aacs", tags=["AAC"])
+app.include_router(stt_router, prefix="/api/v1/stt", tags=["STT"])
 
 @app.get("/")
 def root():
     return {"message": "AI API is running"}
+
