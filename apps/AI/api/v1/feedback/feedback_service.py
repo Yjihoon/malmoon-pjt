724366e0d0a676ff1eef48d52340a25217fb59f6
정수형 <reosis96@gmail.com>
2025-08-11 17:59:39 +0900

    fix: infra 관련 코드 수정

--- apps/AI/api/v1/feedback/feedback_service.py
+++ apps/AI/api/v1/feedback/feedback_service.py
@@ -1,13 +1,14 @@
 import os
-import requests
-from dotenv import load_dotenv
 from typing import List, Tuple
-from apps.AI.api.v1.feedback.feedback_schema import SentencePair, WordsPair
+import requests
+from api.v1.feedback.feedback_schema import SentencePair, WordsPair
+from dotenv import load_dotenv
 
 load_dotenv()
 GMS_API_KEY = os.getenv("GMS_API_KEY")
 GMS_ENDPOINT = os.getenv("GMS_FEEDBACK_URL")
 MODEL = "gpt-4o-mini"
+
 
 def generate_feedback_response(sentences: List[SentencePair]) -> Tuple[float, str]:
     prompt = (
